1f1018fee8e0a0cea3e806f2412ed11d4cf09101
abdo-essam <92235360+abdo-essam@users.noreply.github.com>
2026-05-01 15:36:46 +0000

    chore: auto format and api dump [skip ci]

--- logs-network-okhttp/build.gradle.kts
+++ logs-network-okhttp/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidKotlinMultiplatformLibrary)
@@ -24,7 +21,6 @@ kotlin {
             libs.versions.android.minSdk
                 .get()
                 .toInt()
-
     }
 
     jvm()
